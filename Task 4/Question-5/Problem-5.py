104e8c302d9e746b087a838eac4d469df8d6e655
AbinandhiniDM <99638875+AbinandhiniDM@users.noreply.github.com>
2022-02-23 21:30:44 +0530

    Question 5

--- Task 4/Question-5/Problem-5.py
+++ Task 4/Question-5/Problem-5.py
@@ -1,10 +1,10 @@
 # Question 5
 n=int(input("Enter no.of rows:")) # Getting no.of rows from user
-j=n-1
+x=n-1
 for i in range(0,n):
-    for j in range(0, j): # For space at beginning
+    for j in range(0, x): # For space at beginning
         print(end=" ") 
-    j=j-1
+    x=x-1
     for j in range(0, i+1): # For printing each row
         print("* ", end="") 
     print() # For printing each row at next line
