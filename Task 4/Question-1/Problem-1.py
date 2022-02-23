4d2b71e14ea4a8ae7507c949c149885a9914acfa
AbinandhiniDM <99638875+AbinandhiniDM@users.noreply.github.com>
2022-02-23 18:48:19 +0530

    Add files via upload

---
+++ Task 4/Question-1/Problem-1.py
@@ -0,0 +1,6 @@
+# Question 1
+n1=int(input("Enter 1st number:")) # Getting numbers from user
+n2=int(input("Enter 2nd number:"))
+c=n1+n2 # Adding both the numbers
+print("The sum of two numbers is: ",c) #Printing the result
+
