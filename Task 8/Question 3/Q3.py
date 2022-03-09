8a7a64ebbd68a74f1e1e1d571770869ddf2ba438
AbinandhiniDM <99638875+AbinandhiniDM@users.noreply.github.com>
2022-03-09 20:24:51 +0530

    Add files via upload

---
+++ Task 8/Question 3/Q3.py
@@ -0,0 +1,10 @@
+# Question 3
+
+import numpy as np
+print(0 * np.nan)
+print(np.nan != np.nan) # np.nan is not comparable to np.nan directly
+print(np.inf > np.nan)
+print(np.nan - np.nan)
+print(0.3 == 3 * 0.1) 
+
+
